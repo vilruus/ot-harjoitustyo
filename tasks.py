f733ec88e50a38f7953e6925ead41ec4e759e70e
Ville Ruuskanen <ruusvil@gmail.com>
2022-12-06 22:01:31 +0200

    new task for coverage raport

--- tasks.py
+++ tasks.py
@@ -8,7 +8,10 @@ def start(ctx):
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
-##Todo: Implement coverage
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
